f5e7cd3ad981f849397abbee565f970caa9fba56
s7k-windows <benotimprotant@mail.ru>
2025-04-21 12:58:08 +0300

    Reject only if in excpected hosts

--- logic.py
+++ logic.py
@@ -66,8 +66,9 @@ def rejected_connection(username: str, addr: tuple[str,str]):
     if (prelogic.TCP):
         prelogic.TCP.stop()
         prelogic.TCP = None
-    connection.EXPECTED_HOSTS.remove(addr[0])
-    prelogic.LOG(f"Rejection from {username}:{addr[0]}")
+    if (addr[0] in connection.EXPECTED_HOSTS):
+        connection.EXPECTED_HOSTS.remove(addr[0])
+        prelogic.LOG(f"Rejection from {username}:{addr[0]}")
 
 def reject_connection(username: str, addr: tuple[str,str]):
     if (prelogic.TCP):
@@ -75,8 +76,9 @@ def reject_connection(username: str, addr: tuple[str,str]):
         prelogic.TCP = None
     if (prelogic.UDP):
         prelogic.UDP.send(messages.reject_connection_message(), addr[0], int(addr[1]))
-    if (addr[0] in connection.EXPECTED_HOSTS): connection.EXPECTED_HOSTS.remove(addr[0])
-    prelogic.LOG(f"Request from {username}:{addr[0]} rejected")
+    if (addr[0] in connection.EXPECTED_HOSTS): 
+        connection.EXPECTED_HOSTS.remove(addr[0])
+        prelogic.LOG(f"Request from {username}:{addr[0]} rejected")
 
 def wait_for_reply_or_deny(ip: str, sleeptime: float=5):
     time.sleep(sleeptime)
